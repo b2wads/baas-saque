3739689c478d547f12211cb47e40e245a2ec023d
Dalton Barreto <daltonmatos@gmail.com>
2020-02-26 14:27:00 -0300

    Melhorando AccountClient

--- baas/models.py
+++ baas/models.py
@@ -1,12 +1,15 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Account(BaseModel):
     nome: str
     cpf: str
+    saldo: int
 
 
 class Saque(BaseModel):
     data: str
     valor: int
-    account: Account
+    account: Optional[Account]
