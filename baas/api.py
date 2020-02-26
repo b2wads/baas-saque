92d7a3fd510687ebb2962f17b17c3d6322e34fd7
Dalton Barreto <daltonmatos@gmail.com>
2020-02-26 14:57:10 -0300

    Deixando os endpoints HTTP implementados

--- baas/api.py
+++ baas/api.py
@@ -12,14 +12,16 @@ from baas.services.saque import SaqueService
 @app.route(["/saques/{acc_id}"], type=RouteTypes.HTTP, methods=["POST"])
 @parse_body(Saque)
 @parse_id(str)
-async def saque(acc_id: str, saque: Saque):
-    raise NotImplementedError
+async def saque(acc_id: str, saque: Saque) -> Saque:
+    s = await SaqueService.save_saque(acc_id, saque)
+    return web.json_response(s.dict())
 
 
 @app.route(["/saques/{acc_id}"], type=RouteTypes.HTTP, methods=["GET"])
 @parse_id(str)
-async def lista_saques(req: web.Request) -> List[Saque]:
-    raise NotImplementedError
+async def lista_saques(acc_id: str) -> List[Saque]:
+    saques = SaqueService.list_by_acc_id(acc_id)
+    return web.json_response([s.dict() for s in saques])
 
 
 @app.route(["/health"], type=RouteTypes.HTTP, methods=["GET"])
